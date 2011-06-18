372f7e67ec6b259cb578beb851e0eeec9ae8b43a
U-nic-PC\nic <nic@nic-PC.(none)>
2011-06-18 13:45:47 +0100

    content

--- js/page/universal.js
+++ js/page/universal.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         .click(function(){return false;}).addClass("current-page");
 
     $("#banner a")
-        .filter(function (index){return this.href == window.location.href || this.href == "http://nicnguyen.github.com"; })
+        .filter(function (index){return this.href == window.location.href || window.location.href == "http://nicnguyen.github.com"; })
         .click(function(){return false;}).addClass("current-page-home");
 
     var overSwitch = function(){$("#projects").css("display","block");$("#page").fadeTo(200, 0.3);};
